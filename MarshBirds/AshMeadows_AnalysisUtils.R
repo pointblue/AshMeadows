34fbb6b51043db0865a9068bf08355eeecee03c2
lsalas <lsalas@host.docker.internal>
2020-11-17 18:37:21 -0800

    Annotated code more, and added a brief how-to in ReadMe. Pending: complete the same for the Landbird Indicators.

--- MarshBirds/AshMeadows_AnalysisUtils.R
+++ MarshBirds/AshMeadows_AnalysisUtils.R
@@ -6,6 +6,11 @@
 
 ## This file has utility functions to analyze Ash Meadows secretive marshbird data
 
+## This function retrieves the hour and minute of time of sunrise and sunset for a given date, time, coordinates and time zone
+# df is a data.frame that contains a time variable (by default SamplingEventStartTm), longlat variables (default X_COORD,Y_COORD), a date variable (default SurveyDate), and location variables (default StudyArea, Point)
+#     If the data were downloaded from the AKN using the downloader tool, the default values will work.
+# timezone is a valid (i.e., POSIXct) timezone. By default, the code uses the timezone registered with the operating system. If you are running this in a time zone other than GMT-7 (Mountain time), then use timezone="MST"
+# loc.fields, dat.field, tm.field, and pos.fields indicate the location, date, time and position of the survey point. Leave as-is if downloading from the AKN using bulk downloader.
 get.sunrise.set<-function(df,timezone=Sys.timezone(),
 		loc.fields=c("StudyArea","Point"),dat.field="SurveyDate",tm.field="SamplingEventStartTm",
 		pos.fields=c("X_COORD","Y_COORD"),num.days=1){ 
@@ -47,7 +52,10 @@ get.sunrise.set<-function(df,timezone=Sys.timezone(),
 	return(dff) 
 } 
 
-
+## This function prepares the survey data for analysis with hierarchical imperfect detection models
+# df is the data.frame of observations
+# eff is the survey effort table
+# occ is a boolean indicating if the data should be prepared for occupancy models (default) or abundance models
 prepUDFdata<-function(df,eff,occ=T){
 	#create the wide version per species of StudyArea-Transect-point-year-visit+ObservationCount+jday+SamplingEventStartTm
 	#but first need the effort table...
@@ -89,7 +97,10 @@ prepUDFdata<-function(df,eff,occ=T){
 	return(udflist)
 }
 
-
+## This function calculates the mean and confidence intervals of prediction (occupance or abundance) estimates
+# method refers to estimating via unique values (u) or the delta method (d)
+# df is the data.frame of predictions
+# cdf is the data.frame of prediction confidence limits
 aggregateDensities<-function(df,cdf,method="u"){
 	is.na(df) <- sapply(df, is.infinite)
 	df<-cbind(df,cdf)
@@ -112,7 +123,8 @@ aggregateDensities<-function(df,cdf,method="u"){
 	return(ldf)
 }
 
-
+## This function calculates the geometric mean of the mean and confidence limits of predictions
+# df is the data.frame of predictions
 getGeometricMean<-function(df){
 	df$lgPredicted<-log(df$Predicted)
 	df$lgupper<-log(df$upper)
@@ -128,7 +140,10 @@ getGeometricMean<-function(df){
 	return(rdf)
 }
 
-
+## This function returns the number of detections by species and year
+# df is the data.frame of predictions
+# eff is the effort table
+# fct is the aggregating function (by default the mean)
 getNumDetectionsbyYear<-function(df,eff,fct="mean"){
 	spp<-unique(df$BirdCd)
 	res<-data.frame()
